f1a5dd3358274b755d099a41a1da69da6ec025de
sclouds <769100761@qq.com>
2015-10-23 23:30:34 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+int Classify(char c) {
+  if(c == ' ')
+    return 0;
+  else if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    return 1;
+  else if(c >= '0' && c <= '9')
+    return 2;
+  else if(c == '+' || c == '-' || c == '*' || c == '/' || c == '=')
+    return 3;
+  else if(c == '(' || c == ')')
+    return 4;
+  else
+    return 5;
+}
+
+char ReadNum(FILE* fp) {
+  char c;
+  while ((c = fgetc(fp)) != EOF) {
+    if(c >= '0' && c <= '9')
+      continue;
+    else
+      return c;
+  }
+  return '\0';
+}
+
+char ReadIdentifier(FILE* fp) {
+  char c;
+  while ((c = fgetc(fp)) != EOF) {
+    if((c >= '0' && c <= '9') ||
+       (c >= 'a' && c <= 'z') ||
+       (c >= 'A' && c <= 'Z'))
+      continue;
+    else
+      return c;
+  }
+  return '\0';
+}
+
+int main()
+{
+  FILE* fread = fopen("expression.txt", "r");
+  FILE *fwrite = fopen("result.txt", "w+");
+  char c;
+  int flag;
+  if (!fread) {
+    printf("can't open file\n");
+    return -1;
+  }
+  c = fgetc(fread);
+  while(c != EOF && c != '\n') {
+    flag = Classify(c);
+    switch (flag) {
+    case 0:
+      c = fgetc(fread);
+      break;
+    case 1:
+      c = ReadIdentifier(fread);
+      fprintf(fwrite, "(1,i)\n");
+      break;
+    case 2:
+      c = ReadNum(fread);
+      fprintf(fwrite, "(2,c)\n");
+      break;
+    case 3:
+      fprintf(fwrite, "(2,%c)\n", c);
+      c = fgetc(fread);
+      break;
+    case 4:
+      fprintf(fwrite, "(3,%c)\n", c);
+      c = fgetc(fread);
+      break;
+    }
+  }
+  fclose(fread);
+  fclose(fwrite);
+  return 0;
+}
